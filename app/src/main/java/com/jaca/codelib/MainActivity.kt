476c99003b96f120e5d59917c02d98452a987136
jianning.zhu <jianning.zhu@grabtaxi.com>
2021-08-08 18:04:39 +0800

    (TASK)Apply kotlin-kapt plugin for common module

--- app/src/main/java/com/jaca/codelib/MainActivity.kt
+++ app/src/main/java/com/jaca/codelib/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.jaca.codelib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import com.jaca.codelib.databinding.ActivityMainBinding
