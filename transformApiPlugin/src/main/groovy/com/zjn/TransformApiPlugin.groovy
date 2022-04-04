96605a8b433ba9edc26015645ff7554bdc26bf4b
jianning.zhu <jianning.zhu@grabtaxi.com>
2022-04-04 14:08:52 +0800

    (TASK)Transform Api

--- transformApiPlugin/src/main/groovy/com/zjn/TransformApiPlugin.groovy
+++ transformApiPlugin/src/main/groovy/com/zjn/TransformApiPlugin.groovy
@@ -1,11 +1,15 @@
 package com.zjn
 
+import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class TransformApiPlugin implements Plugin<Project> {
     void apply(Project project) {
-        startApply()
+        // 获取Android扩展
+        def android = project.extensions.getByType(AppExtension)
+        // 注册Transform，其实就是添加了Task
+        android.registerTransform(new InjectTransform(project))
         project.task('hello1') {
             group = "zjn"
             description = "gradle build script demo"
@@ -13,11 +17,5 @@ class TransformApiPlugin implements Plugin<Project> {
                 println "Hello from the com.zjn.TransformApiPlugin"
             }
         }
-    }
-
-    private void startApply(){
-        println '------------------------'
-        println 'apply HelloPlugin'
-        println '------------------------'
     }
 }
